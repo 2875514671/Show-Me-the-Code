3ee2b8e127d28ca35398450da694822d615bf2c8
Allen <ysk19950809@163.com>
2020-08-27 14:47:10 +0800

    First commit 2的38次方

--- Challenge000.py
+++ Challenge000.py
@@ -3,4 +3,17 @@
 # @AUTHOR : yusheng
 # @File : Challenge000.py
 # @Softwore : PyCharm
+"""
+https://www.hackingnote.com/en/python-challenge-solutions/level-0
+38是一个很好的（随机的）示例，它显示了int类型在Python中可以无穷大
+"""
 
+a = 2 ** 38
+print(a)
+
+b = pow(2, 38)
+print(b)
+
+# 乘以2等于将二进制表示左移一
+c = 1 << 38
+print(c)
