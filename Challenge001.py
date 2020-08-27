0b1da61dc9a5d9eb1f73a195319f2c10953f80ea
Allen <ysk19950809@163.com>
2020-08-27 17:23:56 +0800

    Two commit 字符映射转换

---
+++ Challenge001.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# @Time :  15:53
+# @AUTHOR : yusheng
+# @File : Challenge001.py
+# @SoftWore : PyCharm
+"""
+https://www.hackingnote.com/en/python-challenge-solutions/level-1
+https://www.runoob.com/python3/python3-string-maketrans.html
+ord()：字符到整数
+chr()：整数到字符
+"""
+import pysnooper
+
+
+# 1. 循环
+@pysnooper.snoop()
+def string_result(raw):
+    result = ""
+    for c in raw:
+        if 'a' <= c <= 'z':
+            result += chr(((ord(c) + 2) - ord('a')) % 26 + ord('a'))
+        else:
+            result += c
+    print(result)
+    return result
+
+
+# 2. 列表
+@pysnooper.snoop()
+def string_list(raw):
+    result = ''.join([chr(((ord(c) + 2) - ord('a')) % 26 + ord('a')) if 'a' <= c <= 'z' else c for c in raw])
+    print(result)
+    return result
+
+
+# 3. str.maketrans()或bytes.maketrans()
+@pysnooper.snoop()
+def string_maketrans(raw):
+    table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+    result = raw.translate(table)
+    print(result)
+    return result
+
+
+# 4. str.maketrans()
+@pysnooper.snoop()
+def rewrite_maketrans(raw):
+    a = "abcdefghijklmnopqrstuvwxyz,. '()"
+    b = "cdefghijklmnopqrstuvwxyzab,. '()"
+    # c = list(zip(a, b))
+    d = dict(zip(a, b))
+    result = "".join([d[x] for x in raw])
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    # raw = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb " \
+    #       "rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+    raw = "map"
+    string_result(raw)
+    string_list(raw)
+    string_maketrans(raw)
+    rewrite_maketrans(raw)
