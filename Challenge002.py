e858123d2665876639ed0b8a7ba0a52cb3ce98cf
Allen <ysk19950809@163.com>
2020-08-28 14:31:46 +0800

    Three commit 字符查找

--- Challenge002.py
+++ Challenge002.py
@@ -10,44 +10,15 @@ import re
 
 
 @pysnooper.snoop()
-def request_challenge(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
-    except requests.RequestException:
-        return None
-
-
-@pysnooper.snoop()
-def get_text(html):
-    comments = re.findall("<!--(.*?)-->", html, re.DOTALL)
-    data = comments[-1]
-    return data
-
-
-@pysnooper.snoop()
-def search_text(data):
-    count = {}
-    for c in data:
-        count[c] = count.get(c, 0) + 1
-    print(count)
-    return count
-
-
-@pysnooper.snoop()
-def adjustment_text(text):
-    pass
-
-
-
-def main():
-    url = "http://www.pythonchallenge.com/pc/def/ocr.html"
-    html = request_challenge(url)
-    data = get_text(html)
-    text = search_text(data)
-    adjustment_text(text)
+def get_result(url):
+    html = (requests.get(url)).text
+    # html = response.text
+    data = re.findall("<!--(.*?)-->", html, re.DOTALL)[-1]
+    word = "".join(re.findall("[A-Za-z]", data))
+    print(word)
+    return word
 
 
 if __name__ == '__main__':
-    main()
+    url = "http://www.pythonchallenge.com/pc/def/ocr.html"
+    get_result(url)
