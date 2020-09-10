3c263dbdb1ed9d5e2f2c21484e6b1ec3d6285d85
Allen <ysk19950809@163.com>
2020-09-10 16:18:13 +0800

    Four commit 数字匹配

--- Challenge004.py
+++ Challenge004.py
@@ -3,7 +3,10 @@
 # @Time     : 2020-09-04 14:21:27
 # @File     : Challenge004.py
 # @SoftWare : Sublime Text3
-
+"""
+http://www.pythonchallenge.com/pc/def/linkedlist.php
+https://www.hackingnote.com/en/python-challenge-solutions/level-4/
+"""
 
 import requests
 import re
@@ -21,4 +24,5 @@ while True:
     match = pattern.search(html)
     if match is None:
         break
+    # 注意.group(0)将返回与模式匹配的整个文本，而捕获的段将从.group (1) 开始
     num = match.group(1)
