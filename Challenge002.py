c85e1082569daa7c7a2432fd3b8920d835c9dbf4
Allen <ysk19950809@163.com>
2020-09-10 16:13:08 +0800

    Two commit 字符查找

--- Challenge002.py
+++ Challenge002.py
@@ -3,7 +3,11 @@
 # @AUTHOR : yusheng
 # @File : Challenge002.py
 # @SoftWore : PyCharm
-
+"""
+http://www.pythonchallenge.com/pc/def/ocr.html
+https://www.hackingnote.com/en/python-challenge-solutions/level-2
+https://docs.python.org/zh-cn/3/library/re.html
+"""
 import pysnooper
 import requests
 import re
@@ -12,6 +16,7 @@ import re
 @pysnooper.snoop()
 def get_result(url):
     html = (requests.get(url)).text
+    # 默认情况下，点 不匹配\n，因此需要使用 re.DOTALL
     data = re.findall("<!--(.*?)-->", html, re.DOTALL)[-1]
     word = "".join(re.findall("[A-Za-z]", data))
     print(word)
