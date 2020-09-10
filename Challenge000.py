10cedee1d526dc18c57822b7d6544eb70dade775
Allen <ysk19950809@163.com>
2020-09-10 16:01:18 +0800

    First commit 计算

--- Challenge000.py
+++ Challenge000.py
@@ -4,6 +4,7 @@
 # @File : Challenge000.py
 # @SoftWore : PyCharm
 """
+http://www.pythonchallenge.com/pc/def/0.html
 https://www.hackingnote.com/en/python-challenge-solutions/level-0
 38是一个很好的（随机的）示例，它显示了int类型在Python中可以无穷大
 """
