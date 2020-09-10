e18ee00e93176cf2c2d36e6090541c2b17bb82ad
Allen <ysk19950809@163.com>
2020-09-10 16:27:24 +0800

    Six commit 压缩包处理

--- Challenge006.py
+++ Challenge006.py
@@ -4,6 +4,7 @@
 # @Software: Sublime Text3
 # @FileName : Challenge006.py
 """
+http://www.pythonchallenge.com/pc/def/channel.html
 https://www.hackingnote.com/en/python-challenge-solutions/level-6
 ZipFile.comment：与ZIP文件关联的注释。
 ZipInfo.comment：单个存档成员的注释。
@@ -14,29 +15,19 @@ import re
 
 
 def get_zipfile():
-
+    # 首先需要访问http://www.pythonchallenge.com/pc/def/channel.zip 下载一个压缩包
+    # 通过 zipfile 处理压缩包
     f = zipfile.ZipFile(r"E:\IDM\IDM下载\channel.zip")
-
-    print(f.read('redame.txt').decode("utf-8"))
-
+    print(f.read('readme.txt').decode("utf-8"))
     num = '90052'
-
     comments = []
-
     while True:
-
         content = f.read(num + '.txt').decode('utf-8')
-
         comments.append(f.getinfo(num + ".txt").comment.decode('utf-8'))
-
         match = re.search("Next nothing is (\\d+)", content)
-
         if match is None:
-
             break
-
         num = match.group(1)
-
     print("".join(comments))
 
 
