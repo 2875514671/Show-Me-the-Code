578f64d8e1387be6a4ee3be26161f299b27c49a5
Allen <ysk19950809@163.com>
2020-09-10 16:07:37 +0800

    First commit 字符映射转换

--- Challenge001.py
+++ Challenge001.py
@@ -4,6 +4,7 @@
 # @File : Challenge001.py
 # @SoftWore : PyCharm
 """
+http://www.pythonchallenge.com/pc/def/map.html
 https://www.hackingnote.com/en/python-challenge-solutions/level-1
 https://www.runoob.com/python3/python3-string-maketrans.html
 ord()：字符到整数
@@ -14,9 +15,9 @@ import pysnooper
 
 # 1. 循环
 @pysnooper.snoop()
-def string_result(raw):
+def string_result(raws):
     result = ""
-    for c in raw:
+    for c in raws:
         if 'a' <= c <= 'z':
             result += chr(((ord(c) + 2) - ord('a')) % 26 + ord('a'))
         else:
@@ -27,36 +28,36 @@ def string_result(raw):
 
 # 2. 列表
 @pysnooper.snoop()
-def string_list(raw):
-    result = ''.join([chr(((ord(c) + 2) - ord('a')) % 26 + ord('a')) if 'a' <= c <= 'z' else c for c in raw])
+def string_list(raws):
+    result = ''.join([chr(((ord(c) + 2) - ord('a')) % 26 + ord('a')) if 'a' <= c <= 'z' else c for c in raws])
     print(result)
     return result
 
 
 # 3. str.maketrans()或bytes.maketrans()
 @pysnooper.snoop()
-def string_maketrans(raw):
+def string_maketrans(raws):
     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
-    result = raw.translate(table)
+    result = raws.translate(table)
     print(result)
     return result
 
 
 # 4. str.maketrans()
 @pysnooper.snoop()
-def rewrite_maketrans(raw):
+def rewrite_maketrans(raws):
     a = "abcdefghijklmnopqrstuvwxyz,. '()"
     b = "cdefghijklmnopqrstuvwxyzab,. '()"
     # c = list(zip(a, b))
     d = dict(zip(a, b))
-    result = "".join([d[x] for x in raw])
+    result = "".join([d[x] for x in raws])
     print(result)
     return result
 
 
 if __name__ == '__main__':
-    # raw = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb " \
-    #       "rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+    # raw = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr
+    # ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. """
     raw = "map"
     string_result(raw)
     string_list(raw)
